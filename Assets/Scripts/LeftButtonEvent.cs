c7e38809dd9057fbd64fc410d859411b5dae4400
codevoid <codevoid@126.com>
2019-03-11 23:57:27 +0800

    player move

---
+++ Assets/Scripts/LeftButtonEvent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftButtonEvent : MonoBehaviour
+{
+    public float moveRadius = 65;
+
+    public static float horizontal = 0;
+    public static float vertical = 0;
+    
+    bool isPress;
+    void OnPress(bool isPress){
+        this.isPress = isPress;
+        if (!isPress){
+            transform.Find("Inner").localPosition = Vector2.zero;
+            vertical = 0;
+            horizontal = 0;
+        }
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPress){
+            Vector2 vector = UICamera.lastTouchPosition - new Vector2(95,105);
+            float distance2 = vector.x*vector.x+vector.y*vector.y;
+            // Debug.Log(moveRadius);
+            if (distance2 > moveRadius*moveRadius){
+                vector = vector.normalized*moveRadius;
+            }
+            horizontal = vector.x/moveRadius;
+            vertical = vector.y/moveRadius;
+            transform.Find("Inner").localPosition = vector;
+        }
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,20 +7,34 @@ public class PlayerMove : MonoBehaviour
 
     public CharacterController cc;
 
-    float h,v;
+    public Camera mainCamera;
 
     public float speed = 3;
 
     // Update is called once per frame
     void Update()
     {
-        h = Input.GetAxis("Horizontal");
-        v = Input.GetAxis("Vertical");
+        // float h = Input.GetAxis("Horizontal");
+        // float v = Input.GetAxis("Vertical");
+        float h = LeftButtonEvent.horizontal;
+        float v = LeftButtonEvent.vertical;
         if(Mathf.Abs(h)>0.1 || Mathf.Abs(v)>0.1){
-            Vector3 target = new Vector3(h,0,v);
-            transform.LookAt(target+transform.position);
+            /*
+            *人物真正的行走角度是从摄像机来看的坐标
+            因此有必要将获取的方向进行旋转
+            旋转的角度是摄像机在世界的旋转角度
+            rotation的值并不正确，需要使用eulerAngle
+            同时使用的函数也是求欧拉角（大概）
+             */
+            Vector3 target = new Vector3( h,0,v);
+            //Debug.Log("Old "+target);
+            target = Quaternion.Euler(0,mainCamera.transform.eulerAngles.y,0)*target;
+            //Debug.Log("rotationY "+mainCamera.transform.eulerAngles.y);
+            //Debug.Log("New "+target);
+            transform.LookAt(transform.position+target);
             cc.SimpleMove(transform.forward*speed);
-        }
-        
+        } 
+        //Vector3 target = new Vector3(0,0,1);
+        //transform.LookAt(transform.position-target);
     }
 }
