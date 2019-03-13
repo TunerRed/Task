1df830a79ace39842e83912e20b77d0866b4de10
codevoid <codevoid@126.com>
2019-03-13 18:59:09 +0800

    rider Csharp support

---
+++ Assets/Scripts/RightButtonsEvent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightButtonsEvent : MonoBehaviour
+{
+	public Transform player;
+
+	private void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("player").transform;
+	}
+
+	// Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void OnUp()
+    {
+	    player.position += player.forward * 10;
+    }
+    public void OnDown(){
+	    player.position -= player.forward * 10;
+    }
+    public void OnLeft(){
+    }
+    public void OnRight(){
+    }
+    
+}
